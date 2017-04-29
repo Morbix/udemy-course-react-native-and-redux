915fadb31d99e63ea50aa58c04facf78797f1e7e
Henrique Morbin <morbin_@hotmail.com>
2017-04-29 01:45:30 -0300

    Finishing header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,16 +2,31 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 const Header = () => {
-    const { textStyle } = Styles;
-    return (
-        <Text style={textStyle}>Albums!</Text>
-    );
+	const { textStyle, viewStyle } = Styles;
+
+	return (
+		<View style={viewStyle}>
+			<Text style={textStyle}>Albums!</Text>
+		</View>
+	);
 };
 
 const Styles = {
-    textStyle: {
-        fontSize: 20
-    }
+	viewStyle: {
+		backgroundColor: '#F8F8F8',
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: 60,
+		paddingTop: 15, 
+		shadowColor: '#000',
+		shadowOffset: { width: 0, height: 2 },
+		shadowOpacity: 0.2,
+		elevation: 2,
+		position: 'relative',
+	},
+	textStyle: {
+		fontSize: 20,
+	}
 }
 
 export default Header;
