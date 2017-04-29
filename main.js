7cda23700757291a30fb2b519f95a0a6c2b4ff22
Henrique Morbin <morbin_@hotmail.com>
2017-04-29 01:19:21 -0300

    Basically a hello world

--- main.js
+++ main.js
@@ -1,24 +1,11 @@
 import Expo from 'expo';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, AppRegistry } from 'react-native';
 
-class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up main.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+  return (
+    <Text>Some Text</Text>
+  );
+};
 
 Expo.registerRootComponent(App);
