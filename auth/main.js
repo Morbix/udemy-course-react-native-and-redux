153e840999c6629966db6fd9118e60c549dde05a
Henrique Morbin <morbin_@hotmail.com>
2017-05-06 16:22:05 -0300

    starting another app from the course

---
+++ auth/main.js
@@ -0,0 +1,6 @@
+import Expo from 'expo';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import App from './src/components/App';
+
+Expo.registerRootComponent(App);
