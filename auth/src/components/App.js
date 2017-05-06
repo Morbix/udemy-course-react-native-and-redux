0cc414760e793063eac8a8ff4d878a1bbc353faf
Henrique Morbin <morbin_@hotmail.com>
2017-05-06 19:31:43 -0300

    adding firebase configuration

--- auth/src/components/App.js
+++ auth/src/components/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './common';
 
 export default class App extends Component {
+  componentWillMount() {
+    initializeFirebase();
+  }
+
+  initializeFirebase() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDu-pBbCnzg0i8cczs3Tfu-gl--hhpsNJA',
+      authDomain: 'authentication-f3909.firebaseapp.com',
+      databaseURL: 'https://authentication-f3909.firebaseio.com',
+      projectId: 'authentication-f3909',
+      storageBucket: 'authentication-f3909.appspot.com',
+      messagingSenderId: '116978728352'
+    });
+  }
+
   render() {
     return (
       <View>
