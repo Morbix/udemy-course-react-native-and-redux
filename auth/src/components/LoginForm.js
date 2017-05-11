b542356d6623e39a283c892b57a93913dd865ff5
Henrique Morbin <morbin_@hotmail.com>
2017-05-10 23:55:05 -0300

    adding firebase api to sign in

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
 import { Button, Card, CardSection, Input } from './common';
+import firebase from 'firebase';
 
 export default class LoginForm extends Component {
-  state = { email: '', password: '' };
+  state = {
+    email: '',
+    password: '',
+    error: ''
+  };
+
+  onButtonPress() {
+    const { email, password } = this.state;
+
+    this.setState({ error: '' });
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .catch(() => {
+            this.setState({ error: 'Authentication Failed.'});
+          });
+      });
+  }
 
   render() {
     return(
@@ -24,8 +44,12 @@ export default class LoginForm extends Component {
             onChangeText={ password => this.setState({ password })}/>
         </CardSection>
 
+        <Text style={Styles.errorText}>
+          { this.state.error }
+        </Text>
+
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Log in
           </Button>
         </CardSection>
@@ -33,3 +57,11 @@ export default class LoginForm extends Component {
     );
   }
 }
+
+const Styles = {
+  errorText: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
