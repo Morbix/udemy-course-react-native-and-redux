eba64f9e10523a9ca25478079e7749a8c3bd1d37
Henrique Morbin <morbin_@hotmail.com>
2017-05-14 00:51:37 -0300

    mapping state in props

---
+++ tech_stack/src/components/LibraryList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import { connect } from 'react-redux';
+
+class LibraryList extends Component {
+  render() {
+    console.log(this.props);
+    return <Text>Teste</Text>;
+  }
+}
+
+const mapStateToProps = state => {
+  return { libraries: state.libraries };
+};
+
+export default connect(mapStateToProps)(LibraryList);
